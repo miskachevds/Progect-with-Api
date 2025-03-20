5e33a01ae5a6c7fb85ecf4b60d2ba2f73ca9a9bb
Miskachev Dima <miskachevds@yandex.ru>
2025-03-20 15:32:31 +0300

    post

--- src/About.js
+++ src/About.js
@@ -1,17 +1,32 @@
 import { useState,useEffect } from "react"; //хуки state создание состояния функ комп
                                             // effect загрузка данных один раз после рендеринга компонента
+import { Link } from "react-router"; //навлинк = навигация а линк = ссылка
+
 function About(){
     let [posts, setPosts] = useState([]);//созд сост posts изнач массив пустой
                                          //далее изм сост и закинем в setPosts массив данных
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')//один раз загр данные с API jsonplaceholder
-        .then(response => response.json())//ждем когда данные все загр и преобр js методом в json формат
-        .then(data => setPosts(data))//затем данные передаются в setPosts и наз Data
+        .then(response => response.json())//ждем когда данные все загр и преобр объект json,js методом в объект javascript
+        .then(data => setPosts(data))//изм сост posts c пустого массива на данные кот придут по текущ ссылке 
     },[]);                           //мустой массив озн что эф будет выполнен один раз
 
     return(
-        <div>
+        <div className="content"> 
             <h2>About page</h2>
+            <ul style={{display: "block"}}>
+                {
+                    posts.map(post => (
+                        <li key={post.id} >
+                            <Link to={`/about/${post.id}`}>
+                            {post.title}
+                            </Link>
+                            
+                        
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
--- src/AboutInfo.js
+++ src/AboutInfo.js
@@ -1,10 +1,27 @@
 import { useParams } from "react-router-dom";
+import { useState,useEffect } from "react";
 
 function AboutInfo(){
     let {id} = useParams();
+
+    let [post, setPost] = useState([null]);
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)//один раз загр данные с API jsonplaceholder
+        .then(response => response.json())//ждем когда данные все загр и преобр объект json,js методом в объект javascript
+        .then(data => setPost(data))//данные лежат в перем data
+    },[id])
+
     return(
-        <div>
-            {id}
+        <div className="content">
+            {
+                post && (
+                    <>
+                        <h1>{post.title}</h1>
+                        <p>{post.body}</p>
+                    </>
+                )
+            }
         </div>
     )
 }
